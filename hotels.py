74e0253b8cf2a36d1711af6ddea6a47c31b645e2
Dev <mail@example.com>
2025-04-04 00:16:31 +0900

    Implement HotelPATCH schema for PATCH method, added openapi_examples for POST method, paginations for GET method

--- hotels.py
+++ hotels.py
@@ -1,28 +1,38 @@
 from fastapi import APIRouter, Query, Body
-from pydantic import BaseModel
+from schemas.hotels import HotelSchema, HotelPATCH
 
 hotels = [
-    {"id": 1, "title": "Сочи",   "name": "sochi"},
-    {"id": 2, "title": "Дубай",  "name": "dubai"},
-    {"id": 3, "title": "Шанхай", "name": "shanghai"}
+    {"id": 1, "title": "Сочи",              "name": "sochi"},
+    {"id": 2, "title": "Дубай",             "name": "dubai"},
+    {"id": 3, "title": "Шанхай",            "name": "shanghai"},
+    {"id": 4, "title": "Геленджик",         "name": "gelendzhik"},
+    {"id": 5, "title": "Москва",            "name": "moscow"},
+    {"id": 6, "title": "Казанть",           "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург",   "name": "spb"},
 ]
 router = APIRouter(prefix = "/hotels", tags = ["Отели"])
-
-class Hotel(BaseModel):
-    title: str 
-    name: str
 
 @router.get("")
 def get_hotels(
     hotel_id: int | None = Query(None, description = "Идентификатор"),
-    title: str | None = Query(None, description = "Название"),
-    name: str | None = Query(None, description = "Псевдоним"),
+    title: str | None    = Query(None, description = "Название"),
+    name: str | None     = Query(None, description = "Псевдоним"),
+    page: int            = Query(1),
+    per_page: int        = Query(3),
 ):
-
-    return [hotel for hotel in hotels if (not hotel_id or hotel["id"] == hotel_id) and (not title or hotel["title"] == title) and (not name or hotel["name"] == name)]
+    return [hotel for hotel in hotels if (not hotel_id or hotel["id"] == hotel_id) and (not title or hotel["title"] == title) and (not name or hotel["name"] == name)][(page-1)*(per_page):page*per_page]
 
 @router.post("")
-def create_hotel(hotel: Hotel):
+def create_hotel(hotel: HotelSchema = Body(openapi_examples={
+    "1":{"summary": "Сириус0", "value":{
+        "title": "Отель Сириус 5 звезд у моря",
+        "name": "sirius"
+    }},
+    "2": {"summary":"Пекин", "value":{
+        "title": "Отель Пекин 4 звезды",
+        "name": "pekin"
+    }},
+    })):
     global hotels
 
     hotel = {
@@ -51,7 +61,7 @@ def delete_hotel(
 @router.put("/{hotel_id}", summary = "Замена данных об отеле")
 def update_hotel(
     hotel_id: int,
-    hotel: Hotel
+    hotel: HotelSchema
 ):
     global hotels
     for i, h in enumerate(hotels):
@@ -75,17 +85,16 @@ def update_hotel(
 @router.patch("/{hotel_id}", summary = "Модификация данных об отеле")
 def modify_hotel(
     hotel_id: int,
-    title: str | None = None,
-    name: str | None = None
+    hotel: HotelPATCH,
 ):
     global hotels
-    for i, hotel in enumerate(hotels):
-        if hotel["id"] == hotel_id:
-            if title:
-                hotel["title"] = title
+    for i, h  in enumerate(hotels):
+        if h["id"] == hotel_id:
+            if hotel.title:
+                h["title"] = hotel.title
 
-            if name:
-                hotel["name"] = name
+            if hotel.name:
+                h["name"] = hotel.name
 
             return {
                 "status": "OK", 
