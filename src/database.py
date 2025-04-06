97ca7504f6b9b43fec85294fdb95093289206876
Dev <mail@example.com>
2025-04-07 00:57:47 +0900

    Implement alembic creation DB scripts

--- src/database.py
+++ src/database.py
@@ -1,16 +1,26 @@
 import asyncio
 
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src.config import settings 
 
-print(settings.DB_URL)
+#print(settings.DB_URL)
 engine = create_async_engine(settings.DB_URL)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
-async def func():
+class BaseORM(DeclarativeBase):
+    pass
+
+async def func(): #demo engine
     async with engine.begin() as conn:
         res = await conn.execute(text("SELECT version()"))
         print(res.fetchone())
 
-asyncio.run(func())
+
+async def func2(): #demo async_session_maker
+    async with async_session_maker.begin() as session:
+        await session.execute()
+
+#asyncio.run(func())
