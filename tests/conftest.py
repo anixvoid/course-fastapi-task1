9b3805515648fc0867b0307b8f00d4c42479bd02
Denis <denlesson17@gmail.com>
2025-10-15 23:43:25 +0900

    create separate fixture to test MODE == "TEST"

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,11 @@ from src.database           import BaseORM, engine_null_pool
 from src.models             import *
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main():
+async def check_test_mode():
     assert settings.MODE == "TEST"
 
+@pytest.fixture(scope="session", autouse=True)
+async def async_main(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(BaseORM.metadata.drop_all)
         await conn.run_sync(BaseORM.metadata.create_all)
