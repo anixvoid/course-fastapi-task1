734f1bf56cf0024dac8351f4ced2223a4c4a8139
Denis <denlesson17@gmail.com>
2025-10-24 23:58:39 +0900

    Implement register user fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import pytest
+from httpx                  import AsyncClient, ASGITransport
 
 from src.config             import settings
 from src.database           import BaseORM, engine_null_pool
 from src.models             import *
+
+from src.main               import app
 
 @pytest.fixture(scope="session", autouse=True)
 async def check_test_mode():
@@ -13,3 +16,14 @@ async def async_main(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(BaseORM.metadata.drop_all)
         await conn.run_sync(BaseORM.metadata.create_all)
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://") as ac:
+        response = await ac.post(
+            "/auth/register", 
+            json = {
+                "email"     : "kot@pes.com",
+                "password"  : "1234"
+            }
+        )
