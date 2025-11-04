c0b9c601dcc5e9ca7ff38f7940282a208b5a9071
Denis <denlesson17@gmail.com>
2025-11-05 00:21:22 +0900

    Refactor facilities test_api

--- tests/integration_test/facilities/test_api.py
+++ tests/integration_test/facilities/test_api.py
@@ -1,21 +1,25 @@
 from tests.conftest import async_client
 
-async def test_facilities(async_client):
+async def test_post_facilities(async_client):
+    facility_title = "Массаж"
     response = await async_client.post(
         "/facilities",
         json = {
-            "title" : "Аквариум"
+            "title" : facility_title
         }
     )
-    print(f"{response=}")
-
     assert response.status_code == 200
-    assert response.json().get("status") == "OK"
 
+    res = response.json()    
+    assert isinstance(res, dict)
+    assert res.get("status") == "OK"
+    assert res["data"]["title"]  == facility_title
+
+async def test_get_facilities(async_client):
     response = await async_client.get(
         "/facilities"
     )
     print(f"{response=}")
 
     assert response.status_code == 200
-    assert any(map(lambda row: row.get("title") == "Аквариум", response.json()))
+    assert isinstance(response.json(), list)
