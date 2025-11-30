9c621bb07434cc59f0a0851c6a386e78ca4e67d7
Denis <denlesson17@gmail.com>
2025-12-01 00:44:36 +0900

    refactoring test_auth_flow according to the lesson

--- tests/integration_test/auth/test_api.py
+++ tests/integration_test/auth/test_api.py
@@ -1,3 +1,5 @@
+from typing import Literal
+from httpx import AsyncClient
 import pytest
 
 from tests.conftest import test_account, authenticated_async_client
@@ -10,15 +12,18 @@ async def test_auth(authenticated_async_client):
     assert response.status_code == 200
     assert response.json().get("email") == test_account["email"]
 
-@pytest.mark.parametrize("email, password", [
-    ("user1@mail.com", "pa$$word1"),
-    ("user2@mail.com", "pa$$word2"),
-    ("user3@mail.com", "pa$$word3"),
+@pytest.mark.parametrize("email, password, result", [
+    ("user1@mail.com",  "pa$$word1",    200),
+    ("user2@mail.com",  "pa$$word2",    200),
+    ("user3@mail.com",  "pa$$word3",    200),
+    ("error@error",     "pa$$word4",    422),
+    ("error",           "pa$$word5",    422),
 ])
 async def test_register_user(
-    email,
-    password,
-    async_client
+    email       : str,
+    password    : str,
+    result      : int,
+    async_client: AsyncClient
 ):
     response = await async_client.post(
         "/auth/register", 
@@ -28,47 +33,48 @@ async def test_register_user(
         }
     )
 
-    assert response.status_code == 200
+    assert response.status_code == result
 
 @pytest.mark.parametrize("email, password, result", [
-    ("user1@mail.com", "pa$$word1",    200),
-    ("user2@mail.com", "error",        401),
-    ("error@mail.com", "pa$$word3",    401),
+    ("user1@mail.com",  "pa$$word1",    200),
+    ("user2@mail.com",  "error",        401),
+    ("error@mail.com",  "pa$$word3",    401),
+    ("error@error",     "pa$$word4",    422),
+    ("error",           "pa$$word5",    422),
 ])
-async def test_auth_login_logout_user(
-    email,
-    password,
-    result,
-    async_client
+async def test_auth_flow(
+    email       : str,
+    password    : str,
+    result      : str,
+    async_client: AsyncClient
 ):    
-    response = await async_client.post(
+    resp_login = await async_client.post(
         "/auth/login", 
         json = {
             "email":    email,
             "password": password
         }
     )
-
-    assert response.status_code == result
-    if response.status_code == 200:
-        assert response.json().get("access_token")
+    assert resp_login.status_code == result
+    if resp_login.status_code == 200:
+        assert resp_login.json().get("access_token")
         assert async_client.cookies.get("access_token")
 
-        response = await async_client.get(
+        resp_me = await async_client.get(
             "/auth/me"
         )
+        assert resp_me.status_code == 200
+        assert resp_me.json().get("email") == email
 
-        assert response.status_code == 200
-        assert response.json().get("email") == email
-
-        response = await async_client.post(
+        resp_logout = await async_client.post(
             "/auth/logout", 
             json = test_account
-        )
-        assert response.status_code == 200
-        assert response.json().get("status") == "OK"
+        )        
+        assert resp_logout.status_code == 200
+        assert resp_logout.json().get("status") == "OK"
+        assert "access_token" not in async_client.cookies
 
-    response = await async_client.get(
+    resp_me2 = await async_client.get(
         "/auth/me"
     )
-    assert response.status_code == 401
+    assert resp_me2.status_code == 401
